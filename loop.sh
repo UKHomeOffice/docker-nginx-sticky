de8de0e459023b1c02a368145e77efcc02403166
Allan Degnan <allan@adegnan.net>
2016-03-03 14:44:45 +0000

    made sure we only restart on file change

--- loop.sh
+++ loop.sh
@@ -8,6 +8,6 @@ set -e
 while true
 do
     /gen-config.sh
-    cmp --silent /etc/nginx/nginx.conf file || /bin/cp -rf file /etc/nginx/nginx.conf && /usr/sbin/nginx -s reload
+    cmp --silent /etc/nginx/nginx.conf file || $(/bin/cp -rf file /etc/nginx/nginx.conf && /usr/sbin/nginx -s reload)
     sleep 10
 done;
