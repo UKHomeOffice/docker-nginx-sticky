e7300c0bdd44916ced20ba3169b282101d6300fd
Allan Degnan <allan@adegnan.net>
2016-03-03 14:18:12 +0000

    adding end of lines

--- gen-config.sh
+++ gen-config.sh
@@ -4,10 +4,10 @@ set -u
 set -e
 : ${PORT:=8000}
 : ${DOMAIN:=api.${ENVIRONMENT}.svc.cluster.local}
-BACKENDS=$(dig ${DOMAIN} +short | sort |  xargs -I {} echo "        server {}:443")
+BACKENDS=$(dig ${DOMAIN} +short | sort |  xargs -I {} echo "        server {}:443;")
 
 if [[ -z $BACKENDS ]]; then
-    BACKENDS="        server 127.0.0.1:443"
+    BACKENDS="        server 127.0.0.1:443;"
 fi
 
 cat <<- EOF > file
